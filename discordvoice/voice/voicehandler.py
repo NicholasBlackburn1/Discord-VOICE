744106981601a18c19875dc0291d28d26ff5fbd1
NicholasBlackburn1 <nickblackburn02@gmail.com>
2022-04-03 08:20:45 -0400

    created base ini

--- discordvoice/voice/voicehandler.py
+++ discordvoice/voice/voicehandler.py
@@ -3,16 +3,25 @@ voice handler class uwu
 
 """
 from pyVoIP.VoIP import VoIPPhone, InvalidStateError
-
+from utils import consoleLog as log 
 
 
 class voicehandler(object):
 
+    phone : VoIPPhone
 
     # this is for the sip handler when some one dials the number
-    def answer(call): # This will be your callback function for when you receive a phone call.
+    def answer(self,call): # This will be your callback function for when you receive a phone call.
         try:
             call.answer()
             call.hangup()
         except InvalidStateError:
             pass
+        
+    # UwU starts the call
+    def init_Call(self):
+            log.Warning("Starting to connect to server.....")
+            
+            self.phone= VoIPPhone(<SIP Server IP>, <SIP Server Port>, <SIP Server Username>, <SIP Server Password>, callCallback=answer, myIP=<Your computers local IP>)
+            self.phone.start()
+
