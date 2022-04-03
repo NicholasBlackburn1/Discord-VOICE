848bad00c9ada61901488a01cc7040d8d412e6c0
NicholasBlackburn1 <nickblackburn02@gmail.com>
2022-04-03 08:30:51 -0400

    working on main file

--- discordvoice/__main__.py
+++ discordvoice/__main__.py
@@ -2,7 +2,24 @@
 Main file uwu
 """
 
+from utils import consoleLog as log
 
 
+def main():
+    log.PipeLine_init("starting...")
+
+
+    log.Warning("Starting voip connection....")
+    #TODO: add vip connection
+    log.PipeLine_Ok("created voip connection...")
+    
+
+    #TODO: add discord cliet int
+    log.PipeLine_Ok("started discord bot")
+
+
+
+if __name__ =='__main__':
+    main()
 
 
--- discordvoice/voice/voicehandler.py
+++ discordvoice/voice/voicehandler.py
@@ -2,14 +2,15 @@
 voice handler class uwu
 
 """
+import configparser
 from pyVoIP.VoIP import VoIPPhone, InvalidStateError
 from utils import consoleLog as log 
-
+from pathlib import Path
 
 class voicehandler(object):
 
     phone : VoIPPhone
-
+   
     # this is for the sip handler when some one dials the number
     def answer(self,call): # This will be your callback function for when you receive a phone call.
         try:
@@ -17,11 +18,4 @@ class voicehandler(object):
             call.hangup()
         except InvalidStateError:
             pass
-        
-    # UwU starts the call
-    def init_Call(self):
-            log.Warning("Starting to connect to server.....")
-            
-            self.phone= VoIPPhone(<SIP Server IP>, <SIP Server Port>, <SIP Server Username>, <SIP Server Password>, callCallback=answer, myIP=<Your computers local IP>)
-            self.phone.start()
-
+    
